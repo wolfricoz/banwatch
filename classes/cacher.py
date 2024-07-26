245043c7ec81b8936a38de6ba54247be402e705a
wolfricoz <ricoisdenaam@gmail.com>
2024-07-26 23:04:53 +0200

    Added a long-term cache

---
+++ classes/cacher.py
@@ -0,0 +1,260 @@
+"""This class generates the config file, with functions to change and get values from it"""
+import json
+import logging
+import os
+from abc import ABC, abstractmethod
+
+cache_path = "settings/cache.json"
+
+
+class LongTermCache():
+    """This class generates the config file, with functions to change and get values from it"""
+
+    def create(self):
+        """Creates the config"""
+        dictionary = {
+            "bans": {}
+        }
+        json_object = json.dumps(dictionary, indent=4)
+        if os.path.exists(cache_path):
+            return
+        with open(cache_path, "w") as outfile:
+            outfile.write(json_object)
+            logging.info(f"cache created")
+
+
+    def add_ban(self, waitid, dict):
+        """Adds a ban to the cache"""
+        waitid = str(waitid)
+        if not os.path.exists(cache_path):
+            self.create()
+        with open(cache_path) as f:
+            data = json.load(f)
+            print(data["bans"])
+            if waitid in data["bans"]:
+                print("removing")
+                data["bans"].pop(waitid)
+            data["bans"][waitid] = dict
+        with open(cache_path, 'w') as f:
+            json.dump(data, f, indent=4)
+            logging.info(f"ban added to cache")
+
+    def get_ban(self, waitid):
+        """Gets a ban from the cache"""
+        waitid = str(waitid)
+        if not os.path.exists(cache_path):
+            self.create()
+        with open(cache_path) as f:
+            data = json.load(f)
+            print(waitid)
+            print(data["bans"])
+            if waitid not in data["bans"]:
+                return False
+            return data["bans"][waitid]
+
+    def remove_ban(self, waitid):
+        """Removes a ban from the cache"""
+        waitid = str(waitid)
+        if not os.path.exists(cache_path):
+            self.create()
+        with open(cache_path) as f:
+            data = json.load(f)
+            if waitid not in data["bans"]:
+                return False
+            data["bans"].pop(waitid)
+        with open(cache_path, 'w') as f:
+            json.dump(data, f, indent=4)
+            logging.info(f"ban removed from cache")
+        return True
+    # @staticmethod
+    # @abstractmethod
+    # async def create_bot_config():
+    #     """Creates the general config"""
+    #     if not os.path.isdir("settings"):
+    #         os.mkdir("settings")
+    #     dictionary = {
+    #         "blacklist": [],
+    #         "checklist": [
+    #
+    #         ],
+    #     }
+    #     json_object = json.dumps(dictionary, indent=4)
+    #     if os.path.exists(config_path):
+    #         with open(config_path) as f:
+    #             data = json.load(f)
+    #             dictionary = {
+    #                 "blacklist": data.get("blacklist", []),
+    #                 "checklist": data.get("checklist", [])
+    #             }
+    #         with open(config_path, "w") as f:
+    #             json.dump(dictionary, f, indent=4)
+    #         return
+    #     with open(config_path, "w") as outfile:
+    #         outfile.write(json_object)
+    #         logging.info(f"universal config created")
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def create_appeals():
+    #     """Creates the general config"""
+    #     dictionary = {
+    #
+    #     }
+    #     json_object = json.dumps(dictionary, indent=4)
+    #     if os.path.exists(appeals_path):
+    #         return
+    #     with open(appeals_path, "w") as outfile:
+    #         outfile.write(json_object)
+    #         logging.info(f"universal appeals created")
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def add_appeal(userid, guildid, reason):
+    #     """This adds an appeal to the appeals.json file"""
+    #     if not os.path.exists(appeals_path):
+    #         await Configer.create_appeals()
+    #     with open(appeals_path) as f:
+    #         data = json.load(f)
+    #         data[f"{userid}"] = {}
+    #         data[f"{userid}"][f"{guildid}"] = {}
+    #         data[f"{userid}"][f"{guildid}"]["reason"] = str(reason)
+    #         data[f"{userid}"][f"{guildid}"]["status"] = "pending"
+    #
+    #     with open(appeals_path, 'w') as f:
+    #         json.dump(data, f, indent=4)
+    #         logging.info(f"{guildid} added to appeals")
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def get_all_appeals():
+    #     """Gets the appeals from the appeals.json file"""
+    #     if os.path.exists(appeals_path):
+    #         with open(appeals_path) as f:
+    #             data = json.load(f)
+    #             return data
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def get_user_appeals(userid):
+    #     """Gets the appeals from the appeals.json file"""
+    #     if not os.path.exists(appeals_path):
+    #         await Configer.create_appeals()
+    #     with open(appeals_path) as f:
+    #         data = json.load(f)
+    #         if str(userid) not in data.keys():
+    #             return None
+    #         return data[str(userid)]
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def update_appeal_status(userid, guildid, status):
+    #     """Changes the status of an appeal"""
+    #     if status not in ["pending", "approved", "denied"]:
+    #         return
+    #     if not os.path.exists(appeals_path):
+    #         await Configer.create_appeals()
+    #     with open(appeals_path) as f:
+    #         data = json.load(f)
+    #         print(userid, " ", guildid, " ", status)
+    #         data[str(userid)][str(guildid)]["status"] = str(status)
+    #     with open(appeals_path, 'w') as f:
+    #         json.dump(data, f, indent=4)
+    #         logging.info(f"{guildid} changed to {status}")
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def change(guildid, interaction, channelid, key):
+    #     """Changes value in the config"""
+    #     if os.path.exists(f"configs/{guildid}.json"):
+    #         with open(f"configs/{guildid}.json") as f:
+    #             data = json.load(f)
+    #             data[key] = channelid
+    #         with open(f"configs/{guildid}.json", 'w') as f:
+    #             json.dump(data, f, indent=4)
+    #         await interaction.followup.send(f"Config key **{key}** changed to **{channelid}**")
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def get(guildid, key):
+    #     """Gets a value from the config"""
+    #     if os.path.exists(f"configs/{guildid}.json"):
+    #         with open(f"configs/{guildid}.json") as f:
+    #             data = json.load(f)
+    #             return data[key]
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def add_to_blacklist(guildid):
+    #     """Adds a server to the blacklist"""
+    #     if os.path.exists(config_path):
+    #         with open(config_path) as f:
+    #             data = json.load(f)
+    #             data["blacklist"].append(guildid)
+    #         with open(config_path, 'w') as f:
+    #             json.dump(data, f, indent=4)
+    #             logging.info(f"{guildid} added to blacklist")
+    #     else:
+    #         logging.warning("No blacklist found")
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def remove_from_blacklist(guildid):
+    #     """Removes a server from the blacklist"""
+    #     if os.path.exists(config_path):
+    #         with open(config_path) as f:
+    #             data = json.load(f)
+    #             data["blacklist"].remove(guildid)
+    #         with open(config_path, 'w') as f:
+    #             json.dump(data, f, indent=4)
+    #             logging.info(f"{guildid} removed from blacklist")
+    #     else:
+    #         logging.warning("No blacklist found")
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def is_blacklisted(guildid):
+    #     """Checks if a server is blacklisted"""
+    #     if os.path.exists(config_path):
+    #         with open(config_path) as f:
+    #             data = json.load(f)
+    #             if guildid in data["blacklist"]:
+    #                 return True
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def add_checklist(word: str):
+    #     """Adds a word to the checklist"""
+    #     if os.path.exists(config_path):
+    #         with open(config_path) as f:
+    #             data = json.load(f)
+    #             if word in data["checklist"]:
+    #                 return
+    #             data["checklist"].append(word)
+    #         with open(config_path, 'w') as f:
+    #             json.dump(data, f, indent=4)
+    #             logging.info(f"{word} added to checklist")
+    #     else:
+    #         await Configer.create_bot_config()
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def remove_checklist(word: str):
+    #     """Removes a word from the checklist"""
+    #     if os.path.exists(config_path):
+    #         with open(config_path) as f:
+    #             data = json.load(f)
+    #             data["checklist"].remove(word)
+    #         with open(config_path, 'w') as f:
+    #             json.dump(data, f, indent=4)
+    #             logging.info(f"{word} removed from checklist")
+    #     else:
+    #         await Configer.create_bot_config()
+    #
+    # @staticmethod
+    # @abstractmethod
+    # async def get_checklist():
+    #     """Gets the checklist"""
+    #     if os.path.exists(config_path):
+    #         with open(config_path) as f:
+    #             data = json.load(f)
+    #             return data["checklist"]
