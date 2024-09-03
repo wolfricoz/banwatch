ad6c99be2f573630bbbb6e69023e86caf5dd709c
wolfricoz <ricoisdenaam@gmail.com>
2024-09-04 00:12:02 +0200

    Added a task to check for pending bans that may have been missed

--- modules/tasks.py
+++ modules/tasks.py
@@ -1,13 +1,14 @@
 from discord.ext import commands, tasks
 from sqlalchemy.testing.plugin.plugin_base import logging
 
+from classes.bans import Bans
 from classes.blacklist import blacklist_check
 from classes.configer import Configer
 from classes.queue import queue
 from classes.support.discord_tools import send_message
 
 
-class BlacklistTask(commands.Cog):
+class Tasks(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.check_blacklist.start()
@@ -17,6 +18,8 @@ class BlacklistTask(commands.Cog):
 
     @tasks.loop(hours=12)
     async def check_blacklist(self):
+        if self.check_blacklist.current_loop == 0:
+            return
         dev = self.bot.get_channel(int(self.bot.DEV))
         for guild in self.bot.guilds:
             if guild.id == self.bot.SUPPORTGUILD:
@@ -24,8 +27,15 @@ class BlacklistTask(commands.Cog):
             if await blacklist_check(guild, dev):
                 continue
 
+    @tasks.loop(hours=24)
+    async def check_pending_bans(self):
+        await Bans().pending_bans(self.bot)
+
+    @check_pending_bans.before_loop
+    async def before_check_pending_bans(self):
+        await self.bot.wait_until_ready()
 
 
 
 async def setup(bot):
-    await bot.add_cog(BlacklistTask(bot))
+    await bot.add_cog(Tasks(bot))
