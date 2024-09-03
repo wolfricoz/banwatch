cde82c97ce72709289ac1ecca2d16abf3f4366ae
wolfricoz <ricoisdenaam@gmail.com>
2024-09-03 23:58:58 +0200

    Cleaning up on ban can now be toggled!

--- classes/bans.py
+++ classes/bans.py
@@ -245,3 +245,46 @@ class Bans(metaclass=Singleton):
             queue().add(self.search_messages(bot, channel, banid, reason), priority=2)
         channel = bot.get_channel(bot.APPROVALCHANNEL)
         queue().add(self.search_messages(bot, channel, banid, reason), priority=2)
+
+
+    async def pending_bans(self):
+        bans: dict = LongTermCache().get_bans()
+        for ban in bans:
+            reason = bans[ban]['reason']
+            channel = self.bot.get_channel(self.bot.BANCHANNEL)
+            wait_id = ban
+            user_id = int(bans[ban]['user'])
+            guild_id = int(bans[ban]['guild'])
+            user = self.bot.get_user(user_id)
+            guild = self.bot.get_guild(guild_id)
+
+            # If the user is not found in the cache, fetch from the API
+            if user is None:
+                print(f"User with ID {user_id} not found in cache.")
+                try:
+                    user = await self.bot.fetch_user(user_id)
+                except discord.NotFound:
+                    print(f"User with ID {user_id} not found.")
+                except discord.HTTPException as e:
+                    print(f"Failed to fetch user with ID {user_id}: {e}")
+
+            # If the guild is not found in the cache, fetch from the API
+            if guild is None:
+                print(f"Guild with ID {guild_id} not found in cache.")
+                try:
+                    guild = await self.bot.fetch_guild(guild_id)
+                except discord.NotFound:
+                    print(f"Guild with ID {guild_id} not found.")
+                except discord.HTTPException as e:
+                    print(f"Failed to fetch guild with ID {guild_id}: {e}")
+
+            if user is None or guild is None or reason is None:
+                print(f"Error: {user} {guild} {reason}")
+                continue
+            try:
+                embed = discord.Embed(title=f"{user} ({user.id}) was banned in {guild}({guild.owner})",
+                                      description=f"{reason}")
+                embed.set_footer(text=f"/approve_ban {wait_id}")
+                queue().add(channel.send(embed=embed, view=BanApproval(self.bot, wait_id, True)), priority=2)
+            except Exception as e:
+                print(f"Error: {e}")
