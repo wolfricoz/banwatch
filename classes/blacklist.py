8d36120ddcf562d9cd3c61d2bad63885c10c5574
wolfricoz <ricoisdenaam@gmail.com>
2024-09-04 00:53:41 +0200

    Enabling dev mode for leaving (live testing)

--- classes/blacklist.py
+++ classes/blacklist.py
@@ -12,14 +12,14 @@ async def blacklist_check(guild: discord.Guild, log: discord.TextChannel) -> boo
     if await Configer.is_blacklisted(guild.id):
         logging.info(f"Leaving {guild.name} because it is blacklisted")
         queue().add(send_message(log, f"Leaving {guild}({guild.id}) because it is blacklisted"))
-        await guild.leave()
-        # await log.send(f"[DEV MODE] Leaving disabled for testing")
+        # await guild.leave()
+        await log.send(f"[DEV MODE] Leaving disabled for testing")
         return True
     if await Configer.is_user_blacklisted(guild.owner.id):
         logging.info(f"Leaving {guild.name} because the {guild.owner.name}({guild.owner.id}) is blacklisted")
         queue().add(send_message(log, f"Leaving {guild}({guild.id}) because the {guild.owner.name}({guild.owner.id}) is blacklisted"))
         await Configer.add_to_blacklist(guild.id)
-        # await log.send(f"[DEV MODE] Leaving disabled for testing")
-        await guild.leave()
+        await log.send(f"[DEV MODE] Leaving disabled for testing")
+        # await guild.leave()
         return True
     return False
