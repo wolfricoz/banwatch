4cfc093e9e148a7f236d87f541d1a4cfa2d4946c
wolfricoz <ricoisdenaam@gmail.com>
2024-10-21 06:47:06 +0200

    joining module updated to 3.0

--- modules/on_join.py
+++ modules/on_join.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from sqlalchemy.orm import sessionmaker
 
 
-from classes.bans import Bans
+from classes.bans import Bans, DatabaseBans
 from classes.configer import Configer
 
 
@@ -20,14 +20,14 @@ class Events(commands.Cog):
         config = await Configer.get(member.guild.id, "modchannel")
         configid = int(config)
         channel = bot.get_channel(configid)
-        sr = await Bans().check(self.bot, int(member.id))
+        sr = await DatabaseBans().get_user_bans(member.id)
 
         if sr is None:
             logging.info(f"{member} has no ban record")
             return
         if channel is None:
             await member.guild.owner.send('No mod channel set, please set one to receive banwatch notifications')
-        await Bans().send_to_channel(channel, sr, int(member.id))
+        await Bans().send_to_channel(bot, channel, sr, member)
 
 
 async def setup(bot):
