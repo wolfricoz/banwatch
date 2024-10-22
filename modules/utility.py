51723040f42bb754e48a19e829fbc6cd92eb92b6
wolfricoz <ricoisdenaam@gmail.com>
2024-10-22 03:14:32 +0200

    Added Donate command for those who wish to donate.

--- modules/utility.py
+++ modules/utility.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 
 from classes.bans import Bans
 from classes.configer import Configer
+from classes.support.discord_tools import send_response
 from view.buttons.appealbuttons import AppealButtons
 from view.modals import inputmodal
 
@@ -15,10 +16,16 @@ class Utility(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+
     @app_commands.command()
     async def support(self, interaction: discord.Interaction):
         await interaction.response.send_message(
                 "If you are in need of support, please read our documentation at https://wolfricoz.github.io/banwatch/ ! You can find our discord link in the documentation. If you still need help, please join our discord server and ask in the support channel.", ephemeral=True)
+
+    @app_commands.command(name="donate", description="If you like banwatch, consider donating!")
+    async def donate(self, interaction: discord.Interaction):
+        await send_response(interaction, f"If you like the service banwatch provides and would like to financially support banwatch, you can do so here: https://donate.stripe.com/dR6eV63rQfr5g2kcMM")
+
 
     async def autocomplete_appeal(self, interaction: discord.Interaction, text: str) -> typing.List[app_commands.Choice[str]]:
         data = []
