68c187af17ea289263ad734913538ea82cbcdf98
wolfricoz <ricoisdenaam@gmail.com>
2024-10-27 06:30:40 +0100

    Ban approval process step 2: Approval button added

--- view/buttons/banapproval.py
+++ view/buttons/banapproval.py
@@ -19,22 +19,20 @@ class BanApproval(View):
         self.create_thread = create_thread
         self.silent = silent
 
-    @discord.ui.button(label="Verify", style=discord.ButtonStyle.success, custom_id="approve_broadcast")
+    @discord.ui.button(label="Verify", style=discord.ButtonStyle.success, custom_id="verify_ban")
     async def verify(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer(ephemeral=True)
         if self.bot is None or self.wait_id is None:
             await interaction.followup.send("Error: The bot has restarted, the data of this button was lost", ephemeral=True)
             return
         ban_entry = BanDbTransactions().get(self.wait_id, override=True)
-        print(ban_entry)
         if ban_entry is None:
             await interaction.followup.send("Ban not found", ephemeral=True)
-            print("ban not found")
             return
+        # noinspection PyTypeChecker,PydanticTypeChecker
         if len(ban_entry.proof) < 1:
             await interaction.followup.send("No evidence found, please add evidence and try again", ephemeral=True)
             return
-
         guildid = ban_entry.gid
         userid = ban_entry.uid
         reason = ban_entry.reason
@@ -49,9 +47,9 @@ class BanApproval(View):
         await interaction.message.edit(embed=interaction.message.embeds[0])
         if self.silent:
             return
-        await Bans().check_guilds(None, self.bot, guild, user, banembed, self.wait_id, self.create_thread)
+        await Bans().check_guilds(None, self.bot, guild, user, banembed, self.wait_id, self.create_thread, verified=True)
 
-    @discord.ui.button(label="silent", style=discord.ButtonStyle.success, custom_id="approve_no_proof_broadcast")
+    @discord.ui.button(label="approve", style=discord.ButtonStyle.success, custom_id="approve_ban")
     async def approve_no_proof(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer(ephemeral=True)
         if self.bot is None or self.wait_id is None:
@@ -72,9 +70,11 @@ class BanApproval(View):
         invite = await Bans().create_invite(guild)
         banembed.set_footer(text=f"Server Invite: {invite} Server Owner: {owner} ban ID: {self.wait_id} ")
         await interaction.followup.send(f"Approved without proof by {interaction.user.mention}! {'Silent option was true, ban not broadcast' if self.silent else ''}", ephemeral=False)
+        interaction.message.embeds[0].set_footer(text=f"Verified by {interaction.user}")
+        await interaction.message.edit(embed=interaction.message.embeds[0])
         if self.silent:
             return
-        await Bans().check_guilds(interaction, self.bot, guild, user, banembed, self.wait_id, False)
+        await Bans().check_guilds(None, self.bot, guild, user, banembed, self.wait_id, False)
 
     @discord.ui.button(label="Hide", style=discord.ButtonStyle.danger, custom_id="deny_broadcast")
     async def deny(self, interaction: discord.Interaction, button: discord.ui.Button):
