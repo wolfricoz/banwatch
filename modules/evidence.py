57f81c4693db5fdf58d4cc82eed452461479f3cf
wolfricoz <ricoisdenaam@gmail.com>
2024-12-13 20:06:39 +0100

    edited the evidence message

--- modules/evidence.py
+++ modules/evidence.py
@@ -28,7 +28,10 @@ class Evidence(commands.GroupCog, name="evidence"):
                 await send_response(interaction, f"{user.mention} not found in this servers bans")
                 return
 
-        evidence_message = f"Please send a message with the evidence you would like to add to {user.name}'s record, this will be added to the ban ID {ban_id} in our support server. \n Type `cancel` to cancel.\n-# By responding to this message you agree to the evidence being stored in our support server.\n\n**Do __not__ use forwarded messages, as these are currently not supported.**"
+        evidence_message = (f"Please send a message with the evidence you would like to add to {user.name}'s record, this will be added to the ban ID {ban_id} in our support server. \n Type `cancel` to cancel.\n-# By responding to this message you agree to the evidence being stored in our support server."
+                            f"\nPlease avoid uploading:"
+                            f"\n* Personal information (irl information such as a persons name, date of birth, where they live, government documentation, etc)"
+                            f"\n\n**Do __not__ use forwarded messages, as these are currently not supported.**")
         evidence = await await_message(interaction, evidence_message)
         if evidence is False:
             return
