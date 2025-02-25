799c4f513bb317eb3f7495ba51f0ae2d13ccf64a
wolfricoz <ricoisdenaam@gmail.com>
2025-02-25 05:21:53 +0100

    Uploading fix to tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,6 +1,19 @@
+import os
+import sys
 import unittest
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv(os.path.join(os.path.dirname(__file__), 'main.env'))
+
+# Add the parent directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 loader = unittest.TestLoader()
-start_dir = 'test_modules'
+start_dir = os.path.join(os.path.dirname(__file__), 'test_modules')
 suite = loader.discover(start_dir)
 runner = unittest.TextTestRunner()
-runner.run(suite)
+result = runner.run(suite)
+
+# Exit with a non-zero status code if tests failed
+sys.exit(not result.wasSuccessful())
