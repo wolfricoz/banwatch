0dc5ac015c7e008a043d5d688658834b6e9f8e7c
wolfricoz <ricoisdenaam@gmail.com>
2025-02-25 04:50:12 +0100

    Testing new changes

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 loader = unittest.TestLoader()
-start_dir = os.path.abspath('test_modules')
+start_dir = os.path.join(os.path.dirname(__file__), 'test_modules')
 suite = loader.discover(start_dir)
 runner = unittest.TextTestRunner()
 runner.run(suite)
