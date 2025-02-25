416bdb22498c1eb7aac0f648d0f024e3a00e7acc
wolfricoz <ricoisdenaam@gmail.com>
2025-02-25 04:44:19 +0100

    changed test_modules to an absolute path

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,6 +1,7 @@
+import os
 import unittest
 loader = unittest.TestLoader()
-start_dir = 'test_modules'
+start_dir = os.path.abspath('test_modules')
 suite = loader.discover(start_dir)
 runner = unittest.TextTestRunner()
 runner.run(suite)
