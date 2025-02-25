aed70a730c190b70b672cf5de3ad1637bdf6e2a5
wolfricoz <ricoisdenaam@gmail.com>
2025-02-25 04:56:30 +0100

    Testing new changes

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,5 +1,11 @@
 import os
+import sys
 import unittest
+
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), 'main.env'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 loader = unittest.TestLoader()
 start_dir = os.path.join(os.path.dirname(__file__), 'test_modules')
 suite = loader.discover(start_dir)
