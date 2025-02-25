79762b86b1f3a6b8401ae90dbb8f8464c59f2a55
wolfricoz <ricoisdenaam@gmail.com>
2025-02-25 05:24:04 +0100

    Uploading fix to tests

--- database/current.py
+++ database/current.py
@@ -13,98 +13,106 @@ from sqlalchemy_utils import create_database, database_exists
 
 pymysql.install_as_MySQLdb()
 load_dotenv('main.env')
+if os.path.exists("main.env") is False :
+	load_dotenv("tests/main.env")
 DB = os.getenv('DB')
 
 engine = create_engine(f"{DB}?charset=utf8mb4", poolclass=NullPool, echo=False, isolation_level="READ COMMITTED")
-if not database_exists(engine.url):
-    create_database(engine.url)
+if not database_exists(engine.url) :
+	create_database(engine.url)
 
 conn = engine.connect()
 
 
-class Base(DeclarativeBase):
-    pass
-
-class Staff(Base):
-    __tablename__ = "staff"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    uid: Mapped[int] = mapped_column(BigInteger)
-    role: Mapped[str] = mapped_column(String(128))
-
-    def __int__(self):
-        return self.id
-
-class Bans(Base):
-    __tablename__ = "bans"
-    ban_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
-    uid: Mapped[int] = mapped_column(BigInteger)
-    gid: Mapped[int] = mapped_column(BigInteger, ForeignKey("servers.id"))
-    reason: Mapped[str] = mapped_column(String(4096, collation='utf8mb4_unicode_ci'), default="")
-    message: Mapped[int] = mapped_column(BigInteger, nullable=True)
-    approved: Mapped[bool] = mapped_column(Boolean, default=True)
-    verified: Mapped[bool] = mapped_column(Boolean, default=False)
-    hidden: Mapped[bool] = mapped_column(Boolean, default=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    staff: Mapped[str] = mapped_column(String(1024, collation='utf8mb4_unicode_ci'))
-    proof: Mapped[List["Proof"]] = relationship("Proof", back_populates="ban", cascade="all, save-update, merge, delete, delete-orphan")
-    appeals: Mapped[List["Appeals"]] = relationship("Appeals", back_populates="ban",
-                                                    cascade="all, save-update, merge, delete, delete-orphan")
-    guild: Mapped["Servers"] = relationship("Servers", back_populates="bans")
-    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None, nullable=True)
-    def __int__(self):
-        return self.ban_id
+class Base(DeclarativeBase) :
+	pass
 
 
-class Proof(Base):
-    __tablename__ = "proof"
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    ban_id: Mapped[int] = mapped_column(ForeignKey("bans.ban_id"))
-    uid: Mapped[int] = mapped_column(BigInteger)
-    proof: Mapped[str] = mapped_column(String(4096, collation='utf8mb4_unicode_ci'))
-    attachments: Mapped[str] = mapped_column(String(10000, collation='utf8mb4_unicode_ci'))
-    ban: Mapped["Bans"] = relationship("Bans", back_populates="proof", )
+class Staff(Base) :
+	__tablename__ = "staff"
+	id: Mapped[int] = mapped_column(primary_key=True)
+	uid: Mapped[int] = mapped_column(BigInteger)
+	role: Mapped[str] = mapped_column(String(128))
 
-    def get_attachments(self):
-        return json.loads(self.attachments)
+	def __int__(self) :
+		return self.id
 
-    def __int__(self):
-        return self.id
 
-class Servers(Base):
-    __tablename__ = "servers"
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=False)
-    owner: Mapped[str] = mapped_column(String(1024, collation='utf8mb4_unicode_ci'))
-    name: Mapped[str] = mapped_column(String(1024, collation='utf8mb4_unicode_ci'))
-    member_count: Mapped[int] = mapped_column(BigInteger)
-    hidden: Mapped[bool] = mapped_column(Boolean, default=False)
-    invite: Mapped[str] = mapped_column(String(256, collation='utf8mb4_unicode_ci'), default="")
-    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    deleted_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=None, nullable=True)
-    bans: Mapped[List["Bans"]] = relationship("Bans", back_populates="guild", cascade="save-update, merge, delete, delete-orphan")
+class Bans(Base) :
+	__tablename__ = "bans"
+	ban_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+	uid: Mapped[int] = mapped_column(BigInteger)
+	gid: Mapped[int] = mapped_column(BigInteger, ForeignKey("servers.id"))
+	reason: Mapped[str] = mapped_column(String(4096, collation='utf8mb4_unicode_ci'), default="")
+	message: Mapped[int] = mapped_column(BigInteger, nullable=True)
+	approved: Mapped[bool] = mapped_column(Boolean, default=True)
+	verified: Mapped[bool] = mapped_column(Boolean, default=False)
+	hidden: Mapped[bool] = mapped_column(Boolean, default=False)
+	created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+	staff: Mapped[str] = mapped_column(String(1024, collation='utf8mb4_unicode_ci'))
+	proof: Mapped[List["Proof"]] = relationship("Proof", back_populates="ban",
+	                                            cascade="all, save-update, merge, delete, delete-orphan")
+	appeals: Mapped[List["Appeals"]] = relationship("Appeals", back_populates="ban",
+	                                                cascade="all, save-update, merge, delete, delete-orphan")
+	guild: Mapped["Servers"] = relationship("Servers", back_populates="bans")
+	deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None, nullable=True)
 
-    def __int__(self):
-        return self.id
+	def __int__(self) :
+		return self.ban_id
+
+
+class Proof(Base) :
+	__tablename__ = "proof"
+	id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+	ban_id: Mapped[int] = mapped_column(ForeignKey("bans.ban_id"))
+	uid: Mapped[int] = mapped_column(BigInteger)
+	proof: Mapped[str] = mapped_column(String(4096, collation='utf8mb4_unicode_ci'))
+	attachments: Mapped[str] = mapped_column(String(10000, collation='utf8mb4_unicode_ci'))
+	ban: Mapped["Bans"] = relationship("Bans", back_populates="proof", )
+
+	def get_attachments(self) :
+		return json.loads(self.attachments)
+
+	def __int__(self) :
+		return self.id
+
+
+class Servers(Base) :
+	__tablename__ = "servers"
+	id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=False)
+	owner: Mapped[str] = mapped_column(String(1024, collation='utf8mb4_unicode_ci'))
+	name: Mapped[str] = mapped_column(String(1024, collation='utf8mb4_unicode_ci'))
+	member_count: Mapped[int] = mapped_column(BigInteger)
+	hidden: Mapped[bool] = mapped_column(Boolean, default=False)
+	invite: Mapped[str] = mapped_column(String(256, collation='utf8mb4_unicode_ci'), default="")
+	updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+	deleted_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=None, nullable=True)
+	bans: Mapped[List["Bans"]] = relationship("Bans", back_populates="guild",
+	                                          cascade="save-update, merge, delete, delete-orphan")
+
+	def __int__(self) :
+		return self.id
+
 
 class Appeals(Base) :
-    __tablename__ = "appeals"
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
-    ban_id: Mapped[int] = mapped_column(ForeignKey("bans.ban_id"))
-    message: Mapped[str] = mapped_column(String(2000, collation='utf8mb4_unicode_ci'))
-    status: Mapped[str] = mapped_column(Enum('approved', 'pending', 'denied', name='status_enum'), nullable=False,
-                                        default='pending')
-    ban: Mapped["Bans"] = relationship("Bans", back_populates="appeals", )
+	__tablename__ = "appeals"
+	id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+	ban_id: Mapped[int] = mapped_column(ForeignKey("bans.ban_id"))
+	message: Mapped[str] = mapped_column(String(2000, collation='utf8mb4_unicode_ci'))
+	status: Mapped[str] = mapped_column(Enum('approved', 'pending', 'denied', name='status_enum'), nullable=False,
+	                                    default='pending')
+	ban: Mapped["Bans"] = relationship("Bans", back_populates="appeals", )
 
-    def __int__(self):
-        return self.id
-
-
-
-def create_bot_database():
-    Base.metadata.create_all(engine)
+	def __int__(self) :
+		return self.id
 
 
-def drop_bot_database():
-    if os.getenv('DISCORD_TOKEN') is not None:
-        raise Exception("You cannot drop the database while the bot is in production")
-    sessionmaker(engine).close_all()
-    Base.metadata.drop_all(engine)
+def create_bot_database() :
+	Base.metadata.create_all(engine)
+
+
+def drop_bot_database() :
+	if os.getenv('DISCORD_TOKEN') is not None :
+		raise Exception("You cannot drop the database while the bot is in production")
+	sessionmaker(engine).close_all()
+	Base.metadata.drop_all(engine)
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,19 +1,15 @@
 import os
 import sys
 import unittest
+
 from dotenv import load_dotenv
 
-# Load the .env file
 load_dotenv(os.path.join(os.path.dirname(__file__), 'main.env'))
-
-# Add the parent directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 loader = unittest.TestLoader()
 start_dir = os.path.join(os.path.dirname(__file__), 'test_modules')
 suite = loader.discover(start_dir)
 runner = unittest.TextTestRunner()
 result = runner.run(suite)
-
 # Exit with a non-zero status code if tests failed
 sys.exit(not result.wasSuccessful())
