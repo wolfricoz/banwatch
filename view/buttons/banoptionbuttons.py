a4003e758e6b9c6c98fb18a36110827ab26feedf
wolfricoz <ricoisdenaam@gmail.com>
2025-03-09 21:33:37 +0100

    Fix for bug while trying to hide bams.

--- view/buttons/banoptionbuttons.py
+++ view/buttons/banoptionbuttons.py
@@ -8,7 +8,7 @@ from classes.bans import Bans
 from classes.configer import Configer
 from classes.evidence import EvidenceController
 from classes.queue import queue
-from classes.support.discord_tools import await_message, send_message
+from classes.support.discord_tools import await_message, send_message, send_response
 from data.variables.messages import evidence_message_template
 from database.databaseController import ServerDbTransactions
 from view.buttons.banapproval import BanApproval
@@ -29,7 +29,7 @@ class BanOptionButtons(View) :
 		print(ids)
 		self.guild = interaction.guild
 		self.user = interaction.client.get_user(int(ids[1]))
-		if self.user is None:
+		if self.user is None :
 			self.user = await interaction.client.fetch_user(int(ids[1]))
 		self.ban = await self.guild.fetch_ban(self.user)
 		self.wait_id = self.guild.id + self.user.id
@@ -44,7 +44,7 @@ class BanOptionButtons(View) :
 		guild, user, ban = await self.get_data(interaction)
 
 		evidence = await await_message(interaction, evidence_message_template.format(user=user.name, ban_id=self.wait_id))
-		if evidence is False:
+		if evidence is False :
 			return
 		queue().add(self.process(interaction, evidence), priority=2)
 
@@ -56,7 +56,7 @@ class BanOptionButtons(View) :
 	async def silent_with_proof(self, interaction: discord.Interaction, button: button) :
 		guild, user, ban = await self.get_data(interaction)
 		evidence = await await_message(interaction, evidence_message_template.format(user=user.name, ban_id=self.wait_id))
-		if evidence is False:
+		if evidence is False :
 			return
 		queue().add(self.process(interaction, evidence, silent=True), priority=2)
 
@@ -64,7 +64,7 @@ class BanOptionButtons(View) :
 	async def hidden(self, interaction: discord.Interaction, button: button) :
 		await self.process(interaction, hidden=True)
 
-	async def process(self, interaction, evidence= None, hidden=False, silent=False) :
+	async def process(self, interaction, evidence=None, hidden=False, silent=False) :
 		guild, user, ban = await self.get_data(interaction)
 		guild_db = ServerDbTransactions().get(guild.id)
 
@@ -73,21 +73,21 @@ class BanOptionButtons(View) :
 		# check is ban has to be hidden
 		if hidden :
 			queue().add(Bans().add_ban(user.id, guild.id, ban.reason, staff_member.name, hidden=True), priority=2)
-			await interaction.response.send_message(f"Ban for {user.mention} has been successfully hidden.", ephemeral=True)
+			await send_response(interaction, f"Ban for {user.mention} has been successfully hidden.", ephemeral=True)
 			await interaction.message.delete()
 			return
 		# Additional check to see if user is a bot; technically not needed.
 		# check if the ban has a checklisted word
 		checklist_check = await self.check_checklisted_words(ban)
-		if user.bot and not checklist_check:
+		if user.bot and not checklist_check :
 			checklist_check = "User is a bot"
 		word_count = len(ban.reason.split(" ")) < 4
-		if word_count and not checklist_check:
+		if word_count and not checklist_check :
 			checklist_check = "Short ban reason"
 		wait_id = Bans().create_ban_id(user.id, guild.id)
-		if  AccessControl().access_all(user.id) and not checklist_check:
+		if AccessControl().access_all(user.id) and not checklist_check :
 			checklist_check = "Banwatch Staff Member"
-		if checklist_check  :
+		if checklist_check :
 
 			channel = interaction.client.get_channel(int(os.getenv("BANS")))
 			queue().add(Bans().add_ban(user.id, guild.id, ban.reason, staff_member.name, approved=False), priority=2)
@@ -100,11 +100,12 @@ class BanOptionButtons(View) :
 			embed.add_field(name="flagged word", value=checklist_check, inline=False)
 			embed.set_footer(text=f"invite: {guild_db.invite} To approve it manually: /approve_ban {wait_id} ")
 			queue().add(
-				send_message(channel, f"<@&{os.getenv('STAFF_ROLE')}>", embed=embed, view=BanApproval(interaction.client, wait_id, True, silent=silent)))
+				send_message(channel, f"<@&{os.getenv('STAFF_ROLE')}>", embed=embed,
+				             view=BanApproval(interaction.client, wait_id, True, silent=silent)))
 			return
 
 		queue().add(Bans().add_ban(user.id, guild.id, ban.reason, staff_member.name, approved=True), priority=2)
-		if evidence:
+		if evidence :
 			queue().add(self.provide_proof(interaction, evidence), priority=2)
 		if silent :
 			return
