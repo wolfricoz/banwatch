f1bf01dbefdda97d130c81a90a1dc79c2252a122
ricoi <ricoisdenaam@gmail.com>
2025-07-16 22:51:03 +0200

    Tests created for the new features

---
+++ tests/test_modules/test_AppealMsg_transactions.py
@@ -0,0 +1,41 @@
+import unittest
+
+import database.databaseController
+from classes.access import AccessControl
+from database.current import AppealMsgs, Appeals, create_bot_database, drop_bot_database
+from database.databaseController import AppealMsgTransactions, AppealsDbTransactions, StaffDbTransactions, session
+from database.factories.ban import BanFactory
+
+
+class TestAppealMessageDatabaseTransactions(unittest.TestCase) :
+	guild_id = 395614061393477632
+	user_id = 188647277181665280
+	appealMsgController = AppealMsgTransactions()
+
+	def setUp(self) :
+		create_bot_database()
+
+	def tearDown(self) :
+		drop_bot_database()
+
+	def test_create_message(self) :
+		ban = BanFactory().create()
+		appeal = AppealsDbTransactions().add(ban.ban_id, "Test")
+		appealmsg = self.appealMsgController.add("This is a test", self.user_id, self.guild_id, appeal)
+		self.assertIsInstance(appealmsg, AppealMsgs)
+		self.assertEqual(appealmsg.message, "This is a test")
+
+	def test_get_all_messages_of_appeal(self) :
+		ban = BanFactory().create()
+		appeal = AppealsDbTransactions().add(ban.ban_id, "Test")
+		self.appealMsgController.add("This is a test1", self.user_id, self.guild_id, appeal)
+		self.appealMsgController.add("This is a test2", self.guild_id, self.user_id, appeal)
+		self.appealMsgController.add("This is a test3", self.user_id, self.guild_id, appeal)
+		self.appealMsgController.add("This is a test4", self.guild_id, self.user_id, appeal)
+		msgs = self.appealMsgController.get_chat_log(appeal)
+		self.assertEqual(len(msgs), 4)
+		self.assertNotEqual(len(msgs), 2)
+		testString = "This is a test"
+		for position, msg in enumerate(msgs) :
+			self.assertIsInstance(msg, AppealMsgs)
+			self.assertEqual(msg.message, testString + str(position + 1))
