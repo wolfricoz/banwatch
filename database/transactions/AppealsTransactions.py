734ce3be008d271450f97469c37c9e4774817cd1
ricoi <ricoisdenaam@gmail.com>
2026-01-08 21:35:24 +0100

    Creates tests and overhauled appealtransactions table to work with the new system,

--- database/transactions/AppealsTransactions.py
+++ database/transactions/AppealsTransactions.py
@@ -7,9 +7,15 @@ from database.transactions.DatabaseController import DatabaseTransactions
 
 
 class AppealsDbTransactions(DatabaseTransactions) :
-	def get(self, ban_id: int | str) -> Appeals | None :
+	def get(self, ban_id: int | str, current_session = None) -> Appeals | None :
+		if current_session:
+			session = current_session
+			if not ban_id :
+				return None
+			if isinstance(ban_id, str) :
+				ban_id = int(ban_id)
+			return session.scalar(Select(Appeals).where(Appeals.ban_id == ban_id))
 		with self.createsession() as session :
-
 			if not ban_id :
 				return None
 			if isinstance(ban_id, str) :
@@ -31,9 +37,11 @@ class AppealsDbTransactions(DatabaseTransactions) :
 
 	def change_status(self, ban_id, status) :
 		with self.createsession() as session :
-			appeal: Appeals | None = self.get(ban_id)
+			appeal: Appeals | None = self.get(ban_id, session)
 			if not appeal :
+				print("appeal does not exist")
 				return False
 			appeal.status = status
 			self.commit(session)
+			print("status changed")
 			return True
