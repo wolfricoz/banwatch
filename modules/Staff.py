468327b25e814902da83017af4440d79bc223cf5
ricoi <ricoisdenaam@gmail.com>
2026-04-06 21:44:02 +0200

    Added a maximum to the audit bans command.

--- modules/Staff.py
+++ modules/Staff.py
@@ -236,7 +236,7 @@ class Staff(commands.GroupCog, name="staff", description="Commands for BanWatch 
 
 	@app_commands.command(name="audit_bans", description="[Staff command] Audits all or unsent bans to ensure they follow our guidelines")
 	@AccessControl().check_access()
-	async def audit_bans(self, interaction: discord.Interaction, all_bans: bool = False) :
+	async def audit_bans(self, interaction: discord.Interaction, all_bans: bool = False, max: int = 100) :
 		"""
 		Audits all bans or just unsent bans to ensure they meet BanWatch's guidelines and standards. This is a maintenance command for staff to keep the ban database clean and accurate.
 
@@ -249,12 +249,14 @@ class Staff(commands.GroupCog, name="staff", description="Commands for BanWatch 
 		for ban in bans :
 			if ban.message and not all_bans:
 				continue
+			if count >= max :
+				break
 			if count % 50 == 0 :
 				await asyncio.sleep(0)
 				logging.info(f"Auditing bans, current count: {count}/{len(bans)}")
 			queue().add(audit_ban(self.bot, ban), priority=0)
 			count += 1
-		await send_message(interaction.channel, f"Queued {count} bans for audit.")
+		await send_message(interaction.channel, f"Queued {count}/{len(bans)} bans for audit.")
 
 
 
