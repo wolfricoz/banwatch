5273d4653f8bfff5cf4ad50e9be0d60f207318bf
ricoi <ricoisdenaam@gmail.com>
2026-07-12 16:01:27 +0200

    Code review: Cleaned up bans api

--- api/bans.py
+++ api/bans.py
@@ -30,18 +30,18 @@ async def bans_get(ban_request: BanRequest, request: Request) :
 	bans = {ban.ban_id : ban.message for ban in bans if ban.message is not None}
 	if bans is None or len(bans) < 1:
 		logging.warning(f"{request.client.host} failed to connect with {ban_request}, No bans found")
-		return HTTPException(404, detail="No bans found for this user id")
-	logging.info(f"{request.client.host} connected with {ban_request} and returned {bans}")
-	return json.dumps(bans)
+		raise HTTPException(404, detail="No bans found for this user id")
+	logging.info(f"{request.client.host} connected with {ban_request.id} and returned {bans}")
+	return bans
 
 
 @router.get("/bans/count/{user_id}", )
-async def bans_get(user_id: int, request: Request) :
+async def bans_count(user_id: int, request: Request) :
 	# Currently only used by ageverifier, however if this goes public then it needs rate limiting.
 	if not await Auth(request).verify() :
 		# the error is usually raised in the verify function, but this is just a final catch.
 		raise HTTPException(status_code=403)
-	if user_id is None or 16 < user_id < 19:
-		return HTTPException(404)
+	if user_id is None or 16 < len(str(user_id)) < 19:
+		raise HTTPException(404)
 	ban_count = BanTransactions().count_all_user(user_id)
-	return json.dumps({"bans" : ban_count})
+	return {"bans" : ban_count}
